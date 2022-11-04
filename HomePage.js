d91a8d48c8efc7beafef911b2aed03bdfee2690c
Siphosethu-Nyathi <117455427+Siphosethu-Nyathi@users.noreply.github.com>
2022-11-04 05:15:37 -0800

    Add files via upload

---
+++ HomePage.js
@@ -0,0 +1,32 @@
+import '../Styles/HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+        <header className="HomePage-header">
+
+            <header className="introduction">
+              <h1 id="h1-style">Hi, I am <b>Siphosethu Nyathi.</b></h1>  
+              <h2 id="h2-style">A Software Developer Graduate</h2>    
+              <br/>
+            </header>
+            
+            <header className="about-me">
+              <h3 id="h3-style">About Me</h3>
+              <p>I am an IT(Software Developer) Graduate of Nelson Mandela University, with a three year National Diploma and an Advanced Diploma as my highest qualification. I am passionate about Technology and Software Development, and would therefore like to grow within the Tech and Software Development Space. I am eager to learn and be a part of a dynamic growing company.</p>
+              <br/>
+            </header>
+
+            <header className="why-SovTech"> 
+              <h4 id="h4-style">Why I want to be a Developer at SovTech</h4>
+              <p>I would like to be a Developer at SovTech as SovTech is a leading software design and development company that delivers world class technology and service. SovTech is made up of an expert team of elite engineers, designers, coders and industry specialists, the same professionals I will have access to as my mentors through this Graduate Program. I find this program interesting as it is driven by mentorship, on-the-job experience , and engagement with professionals who have years of experience across Software, Business Development and Design. As a SovTech Developer I will be exposed to project diversity and get an opportunity to take on real responsibility as a Software Developer. I believe that SovTech will develop me as a future leader of the business and industry altogether. As someone who is looking for long term placement, I find this program worthwhile as it offers possibility of full-time employment, and will focus on developing technology skills that I need as an aspiring Software Developer such as coding and software/application development, as well as soft skills such as problem-solving, creativity, and communication skills. Lastly, as this program will be a practical, mentored, hands-on first-job environment, it will be highly beneficial to me as I will be taught the foundations of the Tech Industry and how to effectively use and grow my skills.</p>
+            
+              <h5 id="h5-style">I am looking forward to the opportunity to learn, grow, succeed and achieve greatness everyday.</h5>
+            </header>
+        
+        </header>
+    </div>
+  );
+}
+
+export default HomePage;
